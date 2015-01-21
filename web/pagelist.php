302c88e11afb2b51b494aec9c8fc933eda204622
Patrick <mail@minifisch.net>
2015-01-21 19:12:23 +0100

    dashboard & menu
    changed fa-icons in the menu
    updated the dashboard

--- web/pagelist.php
+++ web/pagelist.php
@@ -4,8 +4,8 @@
 	//Register Pages here
 	$pagelist["dashboard"] = new Page("pages/dashboard.php","Dashboard","fa fa-home fa-fw");
 	$pagelist["toplist"] = new Page("pages/toplist.php","Topliste","fa fa-bar-chart fa-fw");
-	$pagelist["player"] = new Page("pages/player.php","Spieler","fa fa-dashboard fa-fw");
-	$pagelist["devstats"] = new Page("pages/devstats.php","Entwicklung","fa fa-dashboard fa-fw");
+	$pagelist["player"] = new Page("pages/player.php","Spieler","fa fa-user fa-fw");
+	$pagelist["devstats"] = new Page("pages/devstats.php","Entwicklung","fa fa-github-alt fa-fw");
 
 	$pagelist["error_404"] = new Page("pages/error_404.php", "404 - Seite nicht gefunden", false);
 	
