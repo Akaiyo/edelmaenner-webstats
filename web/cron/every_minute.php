4eb2d3eb8eeed3e473115e99c85e97e4250d00ed
Patrick <mail@minifisch.net>
2015-01-23 12:32:40 +0100

    fixed the cronjob

--- web/cron/every_minute.php
+++ web/cron/every_minute.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('../config.php');
-require_once('../lib/classes/McMyAdmin.class.php');
+require_once(__DIR__ . '/../config.php');
+require_once(__DIR__ . '/../lib/classes/McMyAdmin.class.php');
 
 $sql = new mysqli($cfg['sql_stats_server'], $cfg['sql_stats_user'], $cfg['sql_stats_password'], $cfg['sql_stats_database']);
 if(!$sql){
@@ -11,11 +11,12 @@ if(!$sql){
 $McMyAdmin = new McMyAdmin('status', 'gpUzgWMeFbuKEvE2xJcj', '144.76.76.163', '8998');
 $McMyAdminServerStatus = $McMyAdmin->getStatus();
 
+
 $users = $McMyAdminServerStatus->users; 
-$maxusers = $McMyAdminServerStatus->maxusers;
-$ram = $McMyAdminServerStatus->ram;
-$maxram = $McMyAdminServerStatus->maxram;
-$cpuusage = $McMyAdminServerStatus->cpuusage;
+$maxusers = $McMyAdminServerStatus->maxusers; 
+$ram = ($McMyAdminServerStatus->ram) ? $McMyAdminServerStatus->ram : -1; 
+$maxram = ($McMyAdminServerStatus->maxram) ? $McMyAdminServerStatus->maxram : -1; 
+$cpuusage = ($McMyAdminServerStatus->cpuusage) ? $McMyAdminServerStatus->cpuusage : -1; 
 
 
 $sql->query("INSERT INTO history (players, maxplayers, ram, maxram, cpuusage) VALUES ($users, $maxusers, $ram, $maxram, $cpuusage)");
